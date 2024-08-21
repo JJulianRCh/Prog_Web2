8a53ab1bf86e6ccde4e0d7a418fd914a1c3e7549
Julian <julianrodriguez27@aragon.unam.mx>
2024-08-21 17:33:14 -0600

    Agregando archivos js

---
+++ clase2108/app2.js
@@ -0,0 +1,7 @@
+let entrada = prompt("Escribe javascript");
+
+if (entrada.toLowerCase() == "javascript") {
+    console.log("Oh genial");
+} else {
+    console.error("ERROR!!");
+}
---
+++ clase2108/operaciones.js
@@ -0,0 +1,25 @@
+let resultado;
+
+//Suma
+resultado = 10 + 12;
+console.log("Resultado:", resultado);
+
+//Resta
+resultado = 14 - 10;
+console.log("Resultado:", resultado);
+
+//Multiplicacion
+resultado = 10 * 10;
+console.log("Resultado:", resultado);
+
+//Division
+resultado = 6 / 2;
+console.log("Resultado:", resultado);
+
+//Residuo
+resultado = 10 % 2;
+console.log("Resultado:", resultado);
+
+//Operadores relacionales
+
+// Mayor 
